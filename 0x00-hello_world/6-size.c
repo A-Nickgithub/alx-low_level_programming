b7b97feed6e4827814bd35a00b4d381fbcd730f0
A-Nickgithub <nicholusalbertmirindo@gmail.com>
2023-10-05 05:34:54 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of char: %zu bytes\n", sizeof(char));
+	printf("Size of short: %zu bytes\n", sizeof(short));
+	printf("Size of int: %zu bytes\n", sizeof(int));
+	printf("Size of long: %zu bytes\n", sizeof(long));
+	printf("Size of long long: %zu bytes\n", sizeof(long long));
+	printf("Size of float: %zu bytes\n", sizeof(float));
+	printf("Size of double: %zu bytes\n", sizeof(double));
+	printf("Size of long double: %zu bytes\n", sizeof(long double));
+	return (0);
+}
