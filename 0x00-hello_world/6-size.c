bb088eb8a56cd5c37514674127204c21db7e9da1
A-Nickgithub <nicholusalbertmirindo@gmail.com>
2023-10-05 05:43:52 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,12 @@
  */
 
 int main(void)
-{
+{       int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+
 	printf("Size of char: %zu bytes\n", sizeof(char));
 	printf("Size of short: %zu bytes\n", sizeof(short));
 	printf("Size of int: %zu bytes\n", sizeof(int));
