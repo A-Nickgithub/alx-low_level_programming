f3f48b15969987c1f627912b6203cb06fbb3cb65
A-Nickgithub <nicholusalbertmirindo@gmail.com>
2023-10-06 06:12:54 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main-main function
+ *
+ * Return:Always 0
+ *
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+			putchar('\n');
+	return (0);
+}
