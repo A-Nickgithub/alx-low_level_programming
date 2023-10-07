9167a3d58339486d4b1cf3866b8288cbc58ad3c6
A-Nickgithub <nicholusalbertmirindo.com>
2023-10-07 15:39:28 +0300

    print numbers updated

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,7 +4,7 @@
 /**
  * main - entry point
  *
- *Declaration: prints all single digit numbers of base 10 starting from 0,
+ *Declaration:irints all single digit numbers of base 10 starting from 0,
  *Return: Always 0
  */
 int main(void)
@@ -12,8 +12,10 @@ int main(void)
 	int n = 0;
 
 	while (n < 10)
+	{
 		printf("%d", n);
 	n++;
+	}
 	printf("\n");
 	return (0);
 }
