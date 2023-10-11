9f979a8197e38fa0872c0c307e721fe7822ea506
A-Nickgithub <nicholusalbertmirindo.com>
2023-10-11 21:30:40 +0300

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *add - this will be used to add two integer values
+ *@a: one of the integer will be passed here
+ *@b: another integer will be passed here
+ *
+ *Return: Always an integer
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
