09ec1a1bd3f0cd88815bb8e99a2aa3d31dadd246
A-Nickgithub <nicholusalbertmirindo.com>
2023-10-11 20:40:45 +0300

    24hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,38 +1,26 @@
 #include "main.h"
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- *jack_bauer - print every minute of the day
+ * jack_bauer - Print every minute of the day
  *
- *Return: void
+ * Return:void
  */
 void jack_bauer(void)
 {
-	int m;
-	int n;
-	int j;
-	int p;
+	int hour;
+	int minute;
 
-	for (m = 0; m <= 2; m++)
+	for (hour = 0; hour < 24; hour++)
 	{
-		for (n = 0; n <= 9; n++)
+		for (minute = 0; minute < 60; minute++)
 		{
-			if ((m <= 1 && n <= 9) || (m <= 2 && n <= 3))
-			{
-				for (j = 0; j <= 5; j++)
-				{
-					for (p = 0; p <= 9; p++)
-					{
-						_putchar(m + '0');
-						_putchar(n + '0');
-						_putchar(58);
-						_putchar(j + '0');
-						_putchar(p + '0');
-						_putchar('\n');
-					}
-				}
-			}
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
