249e6e63616f7d1dbf8ec653328ac274e0b9c1da
A-Nickgithub <nicholusalbertmirindo.com>
2023-10-12 10:34:16 +0300

    multiple integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *mul - to ,ultiply two integers
+ *@a: one of the integer
+ *@b: one of the integer
+ *
+ *Return: Always an integer
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
