7215a3dd88022f9301e5b4984fd264aacac28e20
A-Nickgithub <nicholusalbertmirindo.com>
2023-10-13 15:36:06 +0300

    leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,29 +9,18 @@
  *@year: year
  *Return: void
  */
-
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
-	{
-	if (month >= 2 && day >= 60)
-	{
-	day++;
-	}
+	int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	int day_of_year = 0;
+	int i;
 
-	printf("Day of the year: %d\n", day);
-	printf("Remaining days: %d\n", 366 - day);
-	}
-	else
-	{
-	if (month == 2 && day == 60)
-	{
-	printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-	}
-	else
-	{
-	printf("Day of the year: %d\n", day);
-	printf("Remaining days: %d\n", 365 - day);
-	}
-	}
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+	days_in_month[2] = 29;
+	for (i = 1; i < month; i++)
+	day_of_year += days_in_month[i];
+	day_of_year += day;
+
+	printf("Day of the year: %d\n", day_of_year);
+	printf("Remaining days: %d\n", (days_in_month[month] - day) + 1);
 }
