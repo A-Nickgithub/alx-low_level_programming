5de5e0ff03d37fa9add04e814be42a83d857a69f
A-Nickgithub <nicholusalbertmirindo.com>
2023-10-18 15:29:35 +0300

    print numbers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *print_number - main function
+ *
+ *@n: Function parameter
+ *
+ *Return: Always 0.
+ */
+
+void print_number(int n)
+{
+	unsigned int m;
+
+	m = n;
+
+	if (n < 0)
+	{
+	_putchar('-');
+	m = -n;
+	}
+
+	if (m / 10 != 0)
+	{
+	print_number(m / 10);
+	}
+	_putchar((m % 10) + '0');
+}
