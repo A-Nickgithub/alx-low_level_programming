0a74f4aeb0ba695f54f97fb5a8b77774d22c7a0f
A-Nickgithub <nicholusalbertmirindo.com>
2023-10-27 17:48:33 +0300

    name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,17 +6,15 @@
  *
  *Return: Always 0
  */
-
-int main(int argc, char *argv[])
+int main(__attribute__((unused))int argc, char *argv[])
 {
 	if (argc >= 1)
 	{
-		printf("Program name: %s\n", argv[0]);
+		printf(" %s\n", argv[0]);
 	}
 	else
 	{
-		printf("Unable to determine the program name.\n");
+		printf("\n");
 	}
 	return (0);
 }
-
