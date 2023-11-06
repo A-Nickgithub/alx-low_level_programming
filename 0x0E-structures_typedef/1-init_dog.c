625daae0be0fabbfa9972877e77ff3d8b9c494e1
A-Nickgithub <nicholusalbertmirindo.com>
2023-11-06 19:32:52 +0300

    init dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+/**
+ * init_dog - This func will initialize the member variables
+ * of my structure
+ *
+ * @d: this will point to the address of my_dog while working in here
+ * @name: This is the first member
+ * @age: This is the second member
+ * @owner: Third member
+ *
+ * Return: Nothing
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	}
+}
