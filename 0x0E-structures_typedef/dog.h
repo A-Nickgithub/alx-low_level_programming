aaa2e3eeb396928e2f4bd6ac8c9ef8feaaa699ae
A-Nickgithub <nicholusalbertmirindo.com>
2023-11-06 18:55:43 +0300

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef POPPY
+#define POPPY
+/**
+ * * * struct dog - a structure for my dog
+ * @name: first member to rep name
+ * @age: second member
+ * @owner: Third member
+ *
+ * Description: The work of this structure is to
+ * store the varius attributes of my dog
+ */
+struct dog
+{
+	    char *name;
+	    float age;
+	    char *owner;
+};
+#endif
