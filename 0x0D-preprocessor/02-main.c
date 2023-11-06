1cc14bcfb519189a712e34d2a268388375563c69
A-Nickgithub <nicholusalbertmirindo.com>
2023-11-06 13:09:41 +0300

    2 main

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print the name of the file containing this
+ * source code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("The program was compiled from the file: %s\n", __FILE__);
+	return (0);
+}
+
