a9f663cfe389c299d028d5970757ff9fa174d4dd
A-Nickgithub <nicholusalbertmirindo.com>
2023-11-16 14:43:09 +0300

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * avant_main -executes before the main function executes
+ *
+ * Return: Nothing
+ */
+void avant_main(void)
+{
+		printf("You're beat! and yet, you must allow,\n"
+	"\nI bore my house upon my back!");
+}
