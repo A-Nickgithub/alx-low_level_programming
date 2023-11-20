ac6d9641ff0efa15e4776a0d961b4ab750a91c5b
A-Nickgithub <nicholusalbertmirindo.com>
2023-11-20 12:47:32 +0300

    print listint

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_listint - print the elements in the list
+ * @h: pointer to the first node
+ * Description: will return the number of nodes and
+ * print the contents of the nodes
+ *
+ * Return the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *temp;
+	size_t num = 0;
+
+	temp = h;
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		num++;
+	}
+	return (num);
+}
