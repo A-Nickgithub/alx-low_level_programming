616d5b015587774dfa370084c93c193e8e32f6b9
A-Nickgithub <nicholusalbertmirindo.com>
2024-02-01 14:45:44 +0300

    table-create

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves the value associated
+ * with a key in the hash table.
+ * @ht: The hash table to look into.
+ * @key: The key to search for.
+ *
+ * Return: The value associated with the key,
+ * or NULL if the key couldn't be found.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *temp;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
+	return (NULL);
+}
