54e0925d34e90ef6938b8c130669d28d04bf56d4
A-Nickgithub <nicholusalbertmirindo.com>
2024-03-20 16:20:08 +0300

    5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -22,6 +22,6 @@ def island_perimeter(grid):
                 size += 1
                 if (j > 0 and grid[i][j - 1] == 1):
                     edges += 1
-                    if (i > 0 and grid[i - 1][j] == 1):
-                        edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
     return size * 4 - edges * 2
